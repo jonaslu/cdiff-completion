9006a76bc1a835b1cd40267d53b2d3ed4cec7de9
Jonas Lundberg <jonasl@catchmecode.com>
2016-12-24 23:04:51 +0100

    Prev does not work
    But rest does
    
    Problem with --color= adds a space afterwards

---
+++ cdiff-completion.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+# cdiff parameter-completion
+
+__cdiff()
+{
+    local cur  # Pointer to current completion word.
+
+
+    COMPREPLY=()   # Array variable storing the possible completions.
+    cur=${COMP_WORDS[COMP_CWORD]}
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+
+     case "$prev" in
+        --color=)
+            compgen -o nospace
+            COMPREPLY=( $( compgen -W 'auto always' -- $cur ) )
+            return 0
+            ;;
+    esac
+    case "$cur" in
+        -*)
+            COMPREPLY=( $( compgen -W '-h -s -w -l -c --version --help --side-by-side \
+                --width= --log --color=' -- $cur ) );;
+        --) COMPREPLY=( $( compgen -W '--version --help --side-by-side \
+                --width= --log --color=' -- $cur ) );;
+        # yy)
+        # zz)
+        esac
+
+    return 0
+}
+
+complete -F __cdiff cdiff
