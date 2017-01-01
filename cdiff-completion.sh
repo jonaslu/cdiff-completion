95f0a34bb3364f441bffabba2e27dcfb4b9f1c45
Jonas Lundberg <jonasl@catchmecode.com>
2017-01-01 18:59:58 +0100

    Kind of works with git subcompletion
    Weak points:
    Sourcing in the git log, should find this file
    dynamically.
    
    --c* collides with --cached in git diff (for example).
    
    Need to merge with _git_diff completion on -- and - alternative.
    
    Still need way to not add space when completing --color=

--- cdiff-completion.sh
+++ cdiff-completion.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 # cdiff parameter-completion
 
+. /usr/share/bash-completion/completions/git
+
+containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
 __cdiff()
 {
     local cur prev words cword
@@ -8,15 +16,13 @@ __cdiff()
 
     _get_comp_words_by_ref -n "=" cur prev
 
-    # echo -e "\n cur: ${cur} prev: ${prev} words: ${words} cword: ${cword}"
+    # echo -e "\n cur: ${cur} prev: ${prev} words: ${words[@]} cword: ${cword[@]}"
+    # echo "${COMP_WORDS[@]}"
 
-    # case "$prev" in
-    #     --color=*)
-    #         echo "Getting here"
-    #         COMPREPLY=( $( compgen -W 'auto always' ) )
-    #         return 0
-    #         ;;
-    # esac
+    if containsElement "-l" "${COMP_WORDS[@]}" || containsElement "--log" "${COMP_WORDS[@]}"; then
+        _git_log
+        return 0
+    fi
 
     case "$cur" in
         --color=*)
@@ -33,7 +39,7 @@ __cdiff()
         --*)
             COMPREPLY=( $( compgen -W '--version --help --side-by-side \
                 --width= --log --color=' -- $cur ) );;
-        # *) Add git log completion
+        *) _git_diff
         esac
 
     return 0
